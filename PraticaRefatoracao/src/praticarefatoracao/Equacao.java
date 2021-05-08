33eeb93bf396b82371245031909407ceccbc0762
Grupo8GrupoG <82290835+Grupo8GrupoG@users.noreply.github.com>
2021-05-08 18:14:05 -0300

    Add files via upload

---
+++ PraticaRefatoracao/src/praticarefatoracao/Equacao.java
@@ -0,0 +1,108 @@
+
+package praticarefatoracao;
+
+import javax.swing.JOptionPane;
+
+public class Equacao {
+    private double a;
+    private double b;
+    private double c;
+    private double discriminante;
+
+    
+    public Equacao(){
+        a=0;
+        b=0;
+        c=0;
+        discriminante=0;
+    }
+    
+    public Equacao(double a,double b,double c,double discriminante){
+        this.a=a;
+        this.b=a;
+        this.c=a;
+        this.discriminante=a;
+    }
+    
+    public double getA() {
+        return a;
+    }
+
+    
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    
+    public double getB() {
+        return b;
+    }
+
+    
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    
+    public double getC() {
+        return c;
+    }
+
+    
+    public void setC(double c) {
+        this.c = c;
+    }
+    
+    public void setDiscriminante(double discriminante) {
+        this.discriminante = discriminante;
+    }
+    
+    public double getDiscriminante() {
+        return discriminante;
+    }
+    
+    
+    public void calculaDiscriminante(double a,double b,double c){
+        this.a=a;
+        this.b=b;
+        this.c=c;
+        double discriminante;
+        discriminante=Math.pow(b, 2)-4*a*c;
+        JOptionPane.showMessageDialog(null, "O discriminante é: "+discriminante);
+        this.discriminante=discriminante;
+        
+        if(discriminante<0){
+            JOptionPane.showMessageDialog(null, "Está equação não possui raízes reais");
+        }else if(discriminante==0){
+            JOptionPane.showMessageDialog(null, "Está equação possui duas raízes reais iguais");
+            double x1=(-b+Math.sqrt(discriminante))/2*a;
+            JOptionPane.showMessageDialog(null,String.format( "A raíz da equação são %.3f" ,x1));
+        }
+        else if(discriminante>0){
+            JOptionPane.showMessageDialog(null,"Está equação possui duas raízes reais diferentes");
+            double x1=(-b+Math.sqrt(discriminante))/2*a;
+            double x2=(-b-Math.sqrt(discriminante))/2*a;
+            JOptionPane.showMessageDialog(null,String.format( "As raízes da equação são %.3f  e %.3f" ,x1,x2));
+        } 
+        
+
+        
+    }
+    
+    public void calculaEquacao(double a,double b,double c){
+        
+        if(a==0&&b==0&&c!=0){
+            JOptionPane.showMessageDialog(null,"Coeficientes informados incorretamente");
+        }
+        else if(c==0){
+            JOptionPane.showMessageDialog(null,"Igualdade confirmada 0=0");
+        }
+        
+        if(a==0&&b!=0){
+            JOptionPane.showMessageDialog(null, "Essa é uma equação de primeiro grau");
+            JOptionPane.showMessageDialog(null, "A raíz da equação é: "+b/c);
+        }
+        
+    }
+
+}
---
+++ PraticaRefatoracao/src/praticarefatoracao/PraticaRefatoracao.java
@@ -0,0 +1,35 @@
+package praticarefatoracao;
+
+import javax.swing.JOptionPane;
+
+public class PraticaRefatoracao {
+
+    public static void main(String[] args) {
+        
+        // Integrantes do grupo:Gabriel Asevedo de Amorim RA:42113579 | Gabriel Augusto Ferreira Maia RA:42014524 | Guilherme de Almeida Oliveira RA:42014935 | Guilherme Asevedo de Amorim RA:42015274 
+
+        int vezes = 0;
+
+        vezes = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de vezes a se repetir o programa \nDigite um número negativo para sair", 0));
+        
+        
+        //Fizemos com Programação Orientada a Objeto
+        Equacao equacao = new Equacao();
+        
+        //Adicionamos a opção de repetição ao algoritmo
+        while (vezes > 0) {
+
+            equacao.setA(Double.parseDouble(JOptionPane.showInputDialog("Digite o coeficiente A da equação")));
+            equacao.setB(Double.parseDouble(JOptionPane.showInputDialog("Digite o coeficiente B da equação")));
+            equacao.setC(Double.parseDouble(JOptionPane.showInputDialog("Digite o coeficiente C da equação")));
+
+            equacao.calculaEquacao(equacao.getA(), equacao.getB(), equacao.getC());
+
+            equacao.calculaDiscriminante(equacao.getA(), equacao.getB(), equacao.getC());
+
+            vezes = Integer.parseInt(JOptionPane.showInputDialog("Digite o número de vezes a se repetir o programa \nDigite um número negativo para sair", 0));
+
+        }
+        JOptionPane.showMessageDialog(null, "Programa encerrada");
+    }
+}
