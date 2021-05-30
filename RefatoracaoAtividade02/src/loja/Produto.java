21d872b8d75f6eb7f7ff967e2eb5445ee74d44de
Grupo8GrupoG <82290835+Grupo8GrupoG@users.noreply.github.com>
2021-05-29 23:33:02 -0300

    Add files via upload

---
+++ RefatoracaoAtividade02/src/loja/Produto.java
@@ -0,0 +1,38 @@
+package loja;
+
+public class Produto {
+
+    private String nome;
+    private double quantidade;
+    private double preco;
+
+    public Produto(String nome, double quantidade, double preco) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+}
